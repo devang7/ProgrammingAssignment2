699dcce6a1f64845eca333749d6910a9afdf5519
devang7 <devang.jhaveri@outlook.com>
2015-07-26 01:34:56 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+##this function creates a list of functions, if the matrix is to be initialized for the first time set() should be used.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) 
+{
+  m <- NULL
+  set <- function(y) 
+  {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(x) m <<- x
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns the result when we pass a "special" matix x viz. list that is returned from the above function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       n<-x$getinverse()
+  if(! is.null(n))
+  {
+    print("1")
+    return(n)
+  }
+  else
+  {
+    data <- x$get()
+    m <- solve(data)
+    x$setinverse(m)
+    m
+  }
 }
